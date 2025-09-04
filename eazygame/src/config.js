5b6bf3982a4ebdd991efa1342575490638ed2810
letzho <tzhoji@gmail.com>
2025-09-05 01:24:21 +0800

    Fix API URL using window.location detection

--- eazygame/src/config.js
+++ eazygame/src/config.js
@@ -1,6 +1,11 @@
-const API_BASE_URL = import.meta.env.MODE === 'production'
+// Use window.location to detect if we're on Heroku
+const isProduction = window.location.hostname === 'eazygamenets-3d29b52fe934.herokuapp.com';
+
+const API_BASE_URL = isProduction
   ? 'https://eazygamenets-3d29b52fe934.herokuapp.com'
   : 'http://localhost:3002';
+
+console.log('API_BASE_URL:', API_BASE_URL, 'isProduction:', isProduction);
 
 export default API_BASE_URL;
 
